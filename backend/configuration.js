0a3c685bdf02035533a2d86faa45ae1770b55e3d
Irina Demina <ir-mars@yandex.ru>
2023-07-05 13:03:47 +0600

    правка ревью

--- backend/configuration.js
+++ backend/configuration.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 const BASE_PATH = process.env.BASE_PATH || "https://localhost.ru";
 const DATABASE = process.env.DATABASE || "mongodb://127.0.0.1:27017/mestodb";
 const CODE_JWT =
-  process.env.CODE_JWT ||
-  "5NjYsImV4cCI6MTY4MzAyMzc2Nn0eyJfaWQiOiI2NDQ3OGJkZGRhNzVlYWYxZjZlNDU3YWEiLCJpYXQiOjE2ODI0MTg";
+  process.env.NODE_ENV === "production"
+    ? process.env.CODE_JWT
+    : "super-secret-code";
+console.log(CODE_JWT);
 module.exports = {
   PORT,
   BASE_PATH,
